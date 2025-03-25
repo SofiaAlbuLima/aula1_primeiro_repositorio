ca30d222daa12a8201d1282ec47513033b197849
Sofia Albuquerque Lima <151922342+SofiaAlbuLima@users.noreply.github.com>
2025-03-25 16:52:27 -0300

    inicio aula2

---
+++ Lab-ICC/aula2/binario-pra-decimal.c
@@ -0,0 +1,9 @@
+#include <stdio>
+int main(){
+    int binario;
+    int decimal;
+    printf("Escreva um numero binario de 4 bits para converter para decimal \n");
+    scanf("%4d", &numero);
+
+    printf("Seu numero em decimal é: %4d\n", numero);
+}
